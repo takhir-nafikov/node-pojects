d62c5b767a1c8bec514349d85962a585cea9ebc9
Takhir Nafikov <takhir.learn@gmail.com>
2017-10-17 14:58:54 +0300

    first commit
    initial server

---
+++ Chat/chat.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log(`Сервер запущен на порте: ${app.get('port')}`);
+ });
